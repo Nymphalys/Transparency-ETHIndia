72c16f06012e45535a6638c8a9e7c7504803589b
Nymphalys <kumariladli288@gmail.com>
2022-12-04 08:48:15 +0530

    final

--- src/truffle-contract/truffle-config.js
+++ src/truffle-contract/truffle-config.js
@@ -1,9 +1,32 @@
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // to customize your Truffle configuration!
+//   compilers: {
+//     solc: {
+//       version: "0.4.17"
+//     }
+//   }
+// };
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonic =
+  "law credit romance sell health error couple gate else strike sunny once";
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    matic: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+  },
   compilers: {
     solc: {
-      version: "0.4.17"
-    }
-  }
+      version: "0.4.17",
+    },
+  },
 };
